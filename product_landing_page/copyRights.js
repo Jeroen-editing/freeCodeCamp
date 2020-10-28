0e47a16208b50ea0bdce5e269e2f861e5f3207e0
Jeroen-editing <jdv.editing@gmail.com>
2020-10-28 04:52:08 +0100

    small restyling

--- product_landing_page/copyRights.js
+++ product_landing_page/copyRights.js
@@ -9,7 +9,7 @@ const copyRight = () => {
     let minutes = `${date.getMinutes() < 10 ? '0' : ''}${date.getMinutes()}`;
     let today = `${days[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]}&nbsp;${date.getFullYear()}`;
 
-    document.getElementById("copyRights").innerHTML = `${today}, ${hour}:${minutes} .`;
+    document.getElementById("time").innerHTML = `${today}, ${hour}:${minutes}`;
 }
 
 copyRight();
